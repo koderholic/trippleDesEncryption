6f544e3f30c57ac657c9ad0b50611c2ec49673fc
Oluchi Enebeli <Oluchi.Enebeli@Sterling.ng>
2019-08-27 11:24:14 +0100

    added binaryTobyte, binaryTohex conversion and encrypt/decrypt functions with pkcs7Padding

---
+++ main.go
@@ -0,0 +1,118 @@
+package main
+
+import (
+	"bytes"
+	"crypto/cipher"
+	"encoding/base64"
+	"encoding/hex"
+	"strconv"
+	"crypto/des"
+	"fmt"
+)
+
+func main() {
+	plaintext := "hello"
+	key := "000000010000001000000101000001010000011100001011000011010001000100010010000100010000110100001011000001110000001000000100000010000000000100001100000000110000010100000111000010110000110100011011"
+	iv := "0000000100000010000000110000010100000111000010110000110100010001"
+
+	keyByte := BinaryStringToByte(key)
+	ivByte := BinaryStringToByte(iv)
+	
+	encrypted, errEnc := encrypt(plaintext, keyByte, ivByte)
+	if errEnc != nil {
+		fmt.Println(errEnc)
+	}
+	decrypted, errDec := decrypt(encrypted, keyByte, ivByte)
+	if errDec != nil {
+		fmt.Println(errDec)
+	}
+	fmt.Printf("encrypted is : %s and decrypted is : %s", encrypted, decrypted)
+}
+
+
+func encrypt(plaintext string, key, iv []byte) (string, error) {
+
+	plaintextByte := []byte(plaintext)
+	block, err := des.NewTripleDESCipher(key)
+	if err != nil {
+		return "", nil
+	}
+
+	if len(plaintext)%block.BlockSize() != 0 {
+		plaintextByte = Pad(plaintextByte, block.BlockSize())
+	}
+
+	mode := cipher.NewCBCEncrypter(block, iv)
+	encrypted := make([]byte, len(plaintextByte))
+	mode.CryptBlocks(encrypted, plaintextByte)
+	
+	encryptString := fmt.Sprintf("%x", encrypted)
+	decodedEncryptedHex, err := hex.DecodeString(encryptString)
+	if err != nil {
+		return "", nil
+	}
+	base64Encrypted := base64.StdEncoding.EncodeToString(decodedEncryptedHex)
+
+	return base64Encrypted, nil
+
+}
+
+func decrypt(ciphertext string, key, iv []byte ) (string, error) {
+
+	block, err := des.NewTripleDESCipher(key)
+	if err != nil {
+		return "", nil
+	}
+
+	base64Decoded, err := base64.StdEncoding.DecodeString(ciphertext)
+	if err != nil {
+		return "", nil
+	}
+	decrypter := cipher.NewCBCDecrypter(block, iv)
+	decrypted := make([]byte, len(base64Decoded))
+	decrypter.CryptBlocks(decrypted, base64Decoded)
+	decrypted = UnPad(decrypted)
+
+	return string(decrypted), nil
+}
+
+func Pad(unpadded []byte, blockSize int) []byte {
+	padding := (blockSize - len(unpadded)%blockSize)
+	padtext := bytes.Repeat([]byte{byte(padding)}, padding)
+	return append(unpadded, padtext...)
+}
+
+// Undo removes PKCS7 padding
+func UnPad(padded []byte) []byte {
+	length := len(padded)
+	unpadding := int(padded[length-1])
+	return padded[:(length - unpadding)]
+}
+
+func  BinaryStringToByte(binary string) []byte {
+	var outputByte []byte
+	var str string
+
+	for i := len(binary); i > 0; i -= 8 {
+		if i-8 < 0 {
+			str = string(binary[0:i])
+		} else {
+			str = string(binary[i-8 : i])
+		}
+		v, err := strconv.ParseUint(str, 2, 8)
+		if err != nil {
+			panic(err)
+		}
+		outputByte = append([]byte{byte(v)}, outputByte...)
+	}
+	return outputByte
+}
+
+func BinaryStringToHexByte(binary string) []string {
+	var out []string
+	byteSlice := BinaryStringToByte(binary)
+	for _, b := range byteSlice {
+		out = append(out, "0x"+hex.EncodeToString([]byte{b}))
+	}
+	return out
+}
